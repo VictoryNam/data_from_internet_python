3a73fa655825a0092158cf703194c809958394f4
VictoryNam <viktorianam93@gmail.com>
2021-11-25 12:50:20 +0300

    Lesson_1

--- task_2.py
+++ task_2.py
@@ -1,8 +1,8 @@
 import requests
 import json
 
-token = "02ac83657dccb87789a2ad304fd7396754b1d75a9f6112a496acacaa9a3401c47c2d6d45a346a4bd2916f"
-user_id = 18058451
+token = ""
+user_id = ""
 url = f'https://api.vk.com/method/groups.get?user_ids={user_id}&access_token={token}&v=5.131'
 params = {"extended": 1,
           "fields": "name" "type"}
